f267e5fa501737f76a2bed6f3c942a63987422f5
orlando-messias <programadorlando@gmail.com>
2020-03-06 23:38:34 -0300

    up do arquivo index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const consign = require('consign')
+const bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+
+const app = express()
+app.use(bodyParser.json())  //todo dado via post converte em json
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(expressValidator())
+
+consign().include('routes').include('utils').into(app)  // vai incluir todos os arquivos da pasta q foi especificada
+
+app.listen(3000, () => {
+    console.log('Conectado')
+})
