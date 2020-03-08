7bc33af3f231bf0773ae8c6cb042f7c1eb83dfed
orlando-messias <programadorlando@gmail.com>
2020-03-08 13:48:40 -0300

    update index and DB

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express')  // carrega o express
-const consign = require('consign')  // carrega o consign
+const express = require('express')
+const consign = require('consign')
 const bodyParser = require('body-parser')
 const expressValidator = require('express-validator')
 
 const app = express()
-app.use(bodyParser.json())  //todo dado via post converte em json
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json({limit:'50mb'}))  //todo dado via post converte em json
+app.use(bodyParser.urlencoded({extended: false, 'limit:50mb'}))
 app.use(expressValidator())
 
 consign().include('routes').include('utils').into(app)  // vai incluir todos os arquivos da pasta q foi especificada
 
-app.listen(3000, () => {
+app.listen(4000, () => {
     console.log('Conectado')
 })
