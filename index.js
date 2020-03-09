8fd292c042ef52c1dae796ac7b36886073036039
Orlando <programadorlando@gmail.com>
2020-03-09 12:43:47 -0300

    update property limit from body-parser

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const expressValidator = require('express-validator')
 
 const app = express()
 app.use(bodyParser.json({limit:'50mb'}))  //todo dado via post converte em json
-app.use(bodyParser.urlencoded({extended: false, 'limit:50mb'}))
+app.use(bodyParser.urlencoded({extended: false, limit:'50mb'}))
 app.use(expressValidator())
 
 consign().include('routes').include('utils').into(app)  // vai incluir todos os arquivos da pasta q foi especificada
